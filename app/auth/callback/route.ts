ec169317e5da9b8c7fdae1854d93d19d4595dbff
Protim Ghosh <protim1907@gmail.com>
2026-04-06 08:33:47 -0400

    route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,13 +1,16 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies });
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+
     await supabase.auth.exchangeCodeForSession(code);
   }
 
